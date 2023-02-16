471292a8a3150f943718ea54af216efc92d90b38
Manjaka <manjaka.ysmael@gmail.com>
2023-02-16 15:02:53 +0300

    Server.py tronqué en plusieurs fichiers

--- server.py
+++ server.py
@@ -1,23 +1,20 @@
 import web
+from DB import Db
+from list_artist import artist
 
 web.config.debug = True
 
 urls = (
     '/', 'index',
     '/artist', 'artist', ##url vers la class artistes, à indiquer comme suivant href="/artist"
-    '/album','album' 
+    '/album','album',
+    '/track','track'
 )
 
 class index:
     def GET(self):
-        db = web.database(
-            dbn='mysql',
-            host='tmp-insi.rktmb.org',
-            port=3306,
-            user='insigroup00',
-            pw='insigroup00',
-            db='project00',
-        )
+        #d=Db()
+        db=Db().getDb()
         albums=db.select('Album',limit=10)
         artists=db.select('Artist', limit=10)
         genres=db.select('Genre',limit=10)
@@ -69,93 +66,6 @@ class index:
             result +='<td>'+str(track.UnitPrice)+'</td>'
         result += '</table>'
         result += '</body></html> '
-        return result
-
-###ajout classe artiste url /artist  
-class artist:
-    def GET(self):
-        db = web.database(
-            dbn='mysql',
-            host='tmp-insi.rktmb.org',
-            port=3306,
-            user='insigroup00',
-            pw='insigroup00',
-            db='project00',
-        )
-
-
-        artists=db.select('Artist', limit=10)
-        result='<html><head><title>test</title>'
-        result += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.css"> '
-        result += '</head><body>'
-
-        ##voici les liens
-        result +='<nav class="navbar navbar-expand-sm bg-dark navbar-dark">'
-        result +='<ul class="nav navbar-nav">'
-        result +='<li class="nav-item active"><a class="nav-link" href="/">Home</a></li>'
-        result +='<li class="nav-item"><a class="nav-link" href="/artist">Artists</a></li>'
-        result +='<li class="nav-item"><a class="nav-link" href="/album">Albums</a></li>'
-        result +='<li class="nav-item"><a class="nav-link" href="/track">Tracks</a></li>'
-        result +='</ul>'
-        result +='</nav>'
-        result += '<br>'
-        ##
-        
-        result += '<br>'
-        result += '<h1><dl>listes des artistes</dl></h1>'
-        result += '<table border="1" class="table">'
-        result += '<tr>'
-        result += '<tr class="table-success"><th>Id_artists</th><th>Artiste</th></tr>'
-        for artist in artists:
-            result += '<tr></tr>'
-            result +='<td>'+str(artist.ArtistId)+'</td>'
-            result +='<td>'+artist.Name+'</td>'
-            result += '</tr>'
-        result += '</table>'
-        result += '</body></html>'
-        return result
-###ajout classe album url /album  
-class album:
-    def GET(self):
-        db = web.database(
-            dbn='mysql',
-            host='tmp-insi.rktmb.org',
-            port=3306,
-            user='insigroup00',
-            pw='insigroup00',
-            db='project00',
-        )
-
-
-        albums=db.select('Album', limit=10)
-        result='<html><head><title>test</title>'
-        result += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.css"> '
-        result += '</head><body>'
-
-        ##tous les liens
-        result +='<nav class="navbar navbar-expand-sm bg-dark navbar-dark">'
-        result +='<ul class="nav navbar-nav">'
-        result +='<li class="nav-item active"><a class="nav-link" href="/">Home</a></li>'
-        result +='<li class="nav-item"><a class="nav-link" href="/artist">Artists</a></li>'
-        result +='<li class="nav-item"><a class="nav-link" href="/album">Albums</a></li>'
-        result +='<li class="nav-item"><a class="nav-link" href="/track">Tracks</a></li>'
-        result +='</ul>'
-        result +='</nav>'
-        result += '<br>'
-        ##
-        
-        result += '<br>'
-        result += '<h1><dl>listes des albums</dl></h1>'
-        result += '<table border="1" class="table">'
-        result += '<tr>'
-        result += '<tr class="table-success"><th>Id_albums</th><th>Album</th></tr>'
-        for album in albums:
-            result += '<tr></tr>'
-            result += '<td>'+str(album.AlbumId)+'</td>'
-            result +='<td>'+album.Title+'</td>'
-            result += '</tr>'
-        result += '</table>'
-        result += '</body></html>'
         return result
 if __name__ == "__main__":
     app = web.application(urls, globals())
